52cde5211d6b0ba2490e3921df46c2d8eec90e8a
Kocherizhkin <Kocherizhkin@gmail.com>
2022-08-20 09:49:12 +0300

    Changed text for ZeroDivErr. and added list of buttons to disable

--- config.py
+++ config.py
@@ -2,4 +2,9 @@ NUM_BUTTONS = [f'btn_{num}' for num in range(10)]
 
 MATH_OPERATIONS = ['btn_plus', 'btn_minus', 'btn_mult', 'btn_div']
 
-ERROR_ZERO_DEV = 'Error'
+ERROR_ZERO_DEV = 'ZeroDivErr.'
+
+BUTTONS_TO_DISABLE = [
+    'btn_calc', 'btn_plus', 'btn_minus',
+    'btn_mult', 'btn_div', 'btn_negative', 'btn_point'
+]
